4ff0cd63867120cc7e3987783e1cf56d89d6726c
Espire6525 <deepak.raja@espire.com>
2025-11-13 10:38:35 +0530

    Barnd url chnage for inspire

--- pages/api/generate-summary.js
+++ pages/api/generate-summary.js
@@ -230,7 +230,7 @@ ${contentForAzure}
             blob_url: contentForAzure,
             user_prompt:
               "Rewrite in a more engaging style, but maintain all important details.",
-            brand_url: "https://www.kohler.co.in/press-room/ultimate-smart-press-release",
+            brand_url: "https://careers.inspirebrands.com/life-at-inspire",
             content_type: simplifiedSchema,
           },
           {
